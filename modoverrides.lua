45f9946e8bd9f5718afc806d129f37bc3ad0691e
jangsky215 <339349330@qq.com>
2018-06-29 19:10:35 +0800

    【新档】

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,10 +1,24 @@
 return {
-  ["workshop-1084023218"]={ configuration_options={ Language=true }, enabled=true },
-  ["workshop-1166220990"]={ configuration_options={ attract=2, language=2 }, enabled=true },
-  ["workshop-1181077385"]={ configuration_options={  }, enabled=true },
+  ["-RPG装备鉴定系统-806048964"]={ configuration_options={ Language="en" }, enabled=true },
+  ["-防卡两招-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
   ["workshop-1185229307"]={ configuration_options={  }, enabled=true },
-  ["workshop-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
-  ["workshop-1217385169"]={ configuration_options={  }, enabled=true },
+  ["workshop-1200745268"]={
+    configuration_options={
+      Animal=true,
+      BuildRepair=true,
+      ChopTime=1,
+      CookTime=0,
+      FishTime=0,
+      HSHU=true,
+      MineTime=1,
+      Others=true,
+      Pick=true,
+      QuickDry=true,
+      QuickGrow=0 
+    },
+    enabled=true 
+  },
+  ["workshop-1273791802"]={ configuration_options={  }, enabled=true },
   ["workshop-1275167632"]={
     configuration_options={
       collect_ability="on",
@@ -18,138 +32,55 @@ return {
     },
     enabled=true 
   },
-  ["workshop-1301033176"]={ configuration_options={ LANG="simplified" }, enabled=true },
-  ["workshop-1302830827"]={
-    configuration_options={
-      hallowed_nights=true,
-      winters_feast=true,
-      year_of_the_gobbler=false,
-      year_of_the_varg=true 
-    },
-    enabled=true 
-  },
+  ["workshop-1402200186"]={ configuration_options={ set_idioma="strings" }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
-  ["workshop-439115156"]={
-    configuration_options={
-      avisual_musha="Bmm",
-      avisual_pirate="BL",
-      avisual_pirate_armor="pirate",
-      avisual_princess="WSP",
-      bodyguardwilson=0,
-      craftgems="color",
-      deathPenalty="on",
-      dietmusha="normal",
-      difficultdamage="normal",
-      difficultdamage_range="normal",
-      difficulthealth="normal",
-      difficultmusic="dmusic_normal",
-      difficultover="monster1x",
-      difficultrecipe="normal",
-      difficultsanity="normal",
-      difficultsleep="dsleep_normal",
-      difficulttired="dtired_normal",
-      favoritemusha="normal",
-      frostblade3rd=3,
-      incontainer=0,
-      key=108,
-      key10=110,
-      key11=103,
-      key12=116,
-      key13=282,
-      key14=283,
-      key15=111,
-      key2=114,
-      key3=99,
-      key4=120,
-      key5=107,
-      key6=122,
-      key7=112,
-      key8=118,
-      key9=98,
-      loudlightning="off",
-      modlanguage="chinese",
-      musha_incontainer=1,
-      on_butterfly_shield=1,
-      princess_taste="princess",
-      shareitems=0,
-      smartmusha="normal",
-      stop_spawning=1 
-    },
-    enabled=true 
-  },
   ["workshop-447092740"]={ configuration_options={  }, enabled=true },
-  ["workshop-514078314"]={
+  ["workshop-543945797"]={
     configuration_options={
-      EnableFlooringPlanting=false,
-      EnableGameTurfRecipes=1,
-      EnableIndividualization=true,
-      EnableNonPlayerSpeed=false,
-      EnableTurfedTurfRecipes=1,
-      InsulatingTurfs=0,
-      InsulationDuration=120,
-      RainProtectingTurfs=1,
-      SanityTurfs=0,
-      SpeedyTurfSpeeds=1.25,
-      SpeedyTurfs=1 
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      number_size="default",
+      show_decimal_points="no" 
     },
     enabled=true 
   },
   ["workshop-599498678"]={ configuration_options={  }, enabled=true },
-  ["workshop-623749604"]={
-    configuration_options={
-      Craft="Normal",
-      Destroyable="DestroyByAll",
-      FoodSpoilage=1,
-      Language="TCh",
-      Position="Center",
-      Slots=80 
-    },
-    enabled=true 
-  },
   ["workshop-631648169"]={
     configuration_options={ HuiXueSuDu=1, M_CeShi=false, M_GongSu=false, M_XueLiang=false, QingChuYanShi=1 },
     enabled=true 
   },
+  ["workshop-634670136"]={
+    configuration_options={
+      admin_option=true,
+      cant_destroyby_monster=false,
+      firesuppressor_dig=30,
+      give_start_item=false,
+      is_allow_build_near=false,
+      language="redpig_fun",
+      portal_clear=true,
+      remove_owner_time="never",
+      spread_fire=1,
+      wall_lock=true 
+    },
+    enabled=true 
+  },
   ["workshop-656256171"]={
-    configuration_options={ dont_give_dubloons=2, give_dubloons=0.1, position=1, rec_dubloon=3, rec_gold=3 },
+    configuration_options={ dont_give_dubloons=2, give_dubloons=0, position=1, rec_dubloon=3, rec_gold=3 },
     enabled=true 
   },
   ["workshop-666155465"]={
     configuration_options={ food_estimation=-1, food_order=0, food_style=0, lang="chs", show_food_units=-1 },
     enabled=true 
   },
-  ["workshop-785295023"]={
+  ["workshop-818038080"]={
     configuration_options={
-      bossres=true,
-      companion=true,
-      dist=2.5,
-      language="CHS",
-      minimapicon=false,
-      ownership=0,
-      rebounddmg=0,
-      recipe="normal",
-      recipe_door=true,
-      recipe_fence=true,
-      recipe_tool=false,
-      recipe_vanilla=true,
-      recipe_wall=true 
+      changefestivizerrecipe=0,
+      craftablespecific=true,
+      enableglobalreskins=true,
+      prototyper_module=1 
     },
     enabled=true 
   },
-  ["workshop-812713308"]={ configuration_options={ fireflies_fear=222, language=0 }, enabled=true },
-  ["workshop-829829879"]={ configuration_options={ Language=true, Sound=true }, enabled=true },
-  ["workshop-875994715"]={ configuration_options={ charcoal=true, light=1 }, enabled=true },
-  ["workshop-899583698"]={
-    configuration_options={
-      Dig=false,
-      GameMode=true,
-      Hammer=false,
-      Language=true,
-      Sound=true,
-      TotooriaSpeech=1,
-      UIHeight=80,
-      UIScale=0.75 
-    },
-    enabled=true 
-  } 
+  ["workshop-875994715"]={ configuration_options={ charcoal=true, light=1 }, enabled=true } 
 }
