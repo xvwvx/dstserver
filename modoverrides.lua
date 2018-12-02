b09b3d22e1834443949037e2dfd70bb808d369d5
jangsky215 <339349330@qq.com>
2018-12-03 00:16:52 +0800

    修改

--- modoverrides.lua
+++ modoverrides.lua
@@ -15,7 +15,6 @@ return {
     ["enabled"]=true 
   },
   ["workshop-362175979"]={ ["configuration_options"]={ ["Draw over FoW"]="disabled" }, ["enabled"]=true },
-  ["workshop-369596587"]={ ["configuration_options"]={ ["message_style"]=1 }, ["enabled"]=true },
   ["workshop-374550642"]={ ["configuration_options"]={ ["MAXSTACKSIZE"]=99 }, ["enabled"]=true },
   ["workshop-375850593"]={ ["configuration_options"]={  }, ["enabled"]=true },
   ["workshop-378160973"]={
@@ -88,6 +87,16 @@ return {
       ["position"]=1,
       ["rec_dubloon"]=3,
       ["rec_gold"]=3 
+    },
+    ["enabled"]=true 
+  },
+  ["workshop-666155465"]={
+    ["configuration_options"]={
+      ["food_estimation"]=-1,
+      ["food_order"]=0,
+      ["food_style"]=0,
+      ["lang"]="auto",
+      ["show_food_units"]=-1 
     },
     ["enabled"]=true 
   },
