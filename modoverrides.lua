fcef5a8d792b13ce14a50f3362abf801e1c35360
jangsky215 <339349330@qq.com>
2018-12-02 23:38:33 +0800

    修改

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,6 +1,7 @@
 return {
   ["workshop-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
-  ["workshop-1301033176"]={ configuration_options={ LANG="simplified" }, enabled=true },
+  ["workshop-1273791802"]={ configuration_options={  }, enabled=true },
+  ["workshop-1301033176"]={ configuration_options={ LANG="auto" }, enabled=true },
   ["workshop-1302830827"]={
     configuration_options={
       hallowed_nights=true,
@@ -10,150 +11,17 @@ return {
     },
     enabled=true 
   },
-  ["workshop-1418878027"]={ configuration_options={  }, enabled=true },
-  ["workshop-1484294726"]={ configuration_options={ musica=true, set_idioma="strings" }, enabled=true },
-  ["workshop-1505270912"]={
-    configuration_options={
-      [""]=0,
-      Beach_Biome=true,
-      Jungle_Biome=true,
-      Lavarena_Biome=true,
-      Magma_Biome=true,
-      Meadow_Biome=true,
-      TheGorge_Biome=true,
-      Tidalmarsh_Biome=true,
-      Volcano_Biome=true,
-      ["World Generation"]=5,
-      boatlefthud=0,
-      colourcube=true,
-      hail=true,
-      megarandomCompatibilityWater=false,
-      set_idioma="strings",
-      wind=true 
-    },
-    enabled=true 
-  },
-  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-369596587"]={ configuration_options={ message_style=1 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
-  ["workshop-375859599"]={
-    configuration_options={
-      divider=5,
-      random_health_value=0,
-      random_range=0,
-      show_type=0,
-      unknwon_prefabs=1,
-      use_blacklist=true 
-    },
-    enabled=true 
-  },
-  ["workshop-378160973"]={
-    configuration_options={
-      ENABLEPINGS=true,
-      FIREOPTIONS=2,
-      OVERRIDEMODE=false,
-      SHAREMINIMAPPROGRESS=true,
-      SHOWFIREICONS=true,
-      SHOWPLAYERICONS=true,
-      SHOWPLAYERSOPTIONS=2 
-    },
-    enabled=true 
-  },
-  ["workshop-439115156"]={
-    configuration_options={
-      avisual_musha="Bmm",
-      avisual_pirate="BL",
-      avisual_pirate_armor="pirate",
-      avisual_princess="WSP",
-      bodyguardwilson=0,
-      craftgems="color",
-      deathPenalty="on",
-      dietmusha="normal",
-      difficultdamage="normal",
-      difficultdamage_range="normal",
-      difficulthealth="normal",
-      difficultmusic="dmusic_normal",
-      difficultover="monster1x",
-      difficultrecipe="normal",
-      difficultsanity="normal",
-      difficultsleep="dsleep_normal",
-      difficulttired="dtired_normal",
-      difficultysniff="normal",
-      favoritemusha="normal",
-      frostblade3rd=3,
-      incontainer=0,
-      key=108,
-      key10=110,
-      key11=103,
-      key12=116,
-      key13=282,
-      key14=283,
-      key15=111,
-      key2=114,
-      key3=99,
-      key4=120,
-      key5=107,
-      key6=122,
-      key7=112,
-      key8=118,
-      key9=98,
-      loudlightning="off",
-      modlanguage="chinese",
-      musha_incontainer=1,
-      on_butterfly_shield=1,
-      princess_taste="princess",
-      shareitems=0,
-      smartmusha="normal",
-      stop_spawning=1 
-    },
-    enabled=true 
-  },
-  ["workshop-458940297"]={
-    configuration_options={
-      DFV_ClientPrediction="default",
-      DFV_FueledSettings="default",
-      DFV_Language="EN",
-      DFV_MinimalMode="default",
-      DFV_PercentReplace="default",
-      DFV_ShowACondition="default",
-      DFV_ShowADefence="default",
-      DFV_ShowAType="default",
-      DFV_ShowDamage="default",
-      DFV_ShowFireTime="default",
-      DFV_ShowInsulation="default",
-      DFV_ShowTemperature="default",
-      DFV_ShowUses="default" 
-    },
-    enabled=true 
-  },
-  ["workshop-514078314"]={
-    configuration_options={
-      EnableCosmeticMode=0,
-      EnableFlooringPlanting=false,
-      EnableGameTurfRecipes=1,
-      EnableIndividualization=true,
-      EnableNonPlayerSpeed=false,
-      EnableTurfedTurfRecipes=1,
-      InsulatingTurfs=1,
-      InsulationDuration=480,
-      RainProtectingTurfs=1,
-      SanityTurfs=1,
-      SpeedyTurfSpeeds=1.25,
-      SpeedyTurfs=1 
-    },
-    enabled=true 
-  },
-  ["workshop-543945797"]={
-    configuration_options={
-      amount_of_numbers="default",
-      display_mode="waving",
-      dmg_only="on",
-      number_size="default",
-      show_decimal_points="no" 
-    },
+  ["workshop-447092740"]={ configuration_options={  }, enabled=true },
+  ["workshop-501385076"]={ configuration_options={  }, enabled=true },
+  ["workshop-631648169"]={
+    configuration_options={ HuiXueSuDu=1, M_CeShi=false, M_GongSu=false, M_XueLiang=false, QingChuYanShi=1 },
     enabled=true 
   },
   ["workshop-656256171"]={
-    configuration_options={ dont_give_dubloons=2, give_dubloons=0, position=1, rec_dubloon=3, rec_gold=3 },
+    configuration_options={ dont_give_dubloons=2, give_dubloons=1, position=1, rec_dubloon=3, rec_gold=3 },
     enabled=true 
   },
   ["workshop-697356989"]={
@@ -163,6 +31,25 @@ return {
       wilburrun="y",
       woodlegs_luck=6,
       woodlegs_sanity=-0.08 
+    },
+    enabled=true 
+  },
+  ["workshop-785295023"]={
+    configuration_options={
+      bossres=true,
+      companion=true,
+      dist=2.5,
+      healthmul=-1,
+      language="AUTO",
+      minimapicon=false,
+      ownership=0,
+      rebounddmg=0,
+      recipe="normal",
+      recipe_door=true,
+      recipe_fence=true,
+      recipe_tool=true,
+      recipe_vanilla=false,
+      recipe_wall=true 
     },
     enabled=true 
   },
