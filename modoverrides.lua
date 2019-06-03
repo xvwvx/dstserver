53ac49e19e63a34af408389ca2b501e860a5a845
xvwvx <339349330@qq.com>
2019-06-03 21:59:07 +0800

    Update modoverrides.lua

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,151 +1,114 @@
 return {
-  ["workshop-1216718131"]={
-    ["configuration_options"]={ ["clean"]=true, ["lang"]=true, ["stack"]=true },
-    ["enabled"]=true 
-  },
-  ["workshop-1273791802"]={ ["configuration_options"]={  }, ["enabled"]=true },
-  ["workshop-1275167632"]={
-    ["configuration_options"]={
-      ["collect_ability"]="on",
-      ["container_size"]="9",
-      ["hotkey_mypet"]="on",
-      ["level_system"]="on",
-      ["modlanguage"]="chinese",
-      ["personal_machine"]="on",
-      ["recipe_critterlab"]="off",
-      ["unique_skill"]="on" 
-    },
-    ["enabled"]=true 
-  },
-  ["workshop-1301033176"]={ ["configuration_options"]={ ["LANG"]="auto" }, ["enabled"]=true },
+  ["workshop-1166220990"]={ configuration_options={ attract=2, language=2 }, enabled=true },
+  ["workshop-1181077385"]={ configuration_options={  }, enabled=true },
+  ["workshop-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
+  ["workshop-1217385169"]={ configuration_options={  }, enabled=true },
   ["workshop-1302830827"]={
-    ["configuration_options"]={
-      ["hallowed_nights"]=true,
-      ["winters_feast"]=true,
-      ["year_of_the_gobbler"]=true,
-      ["year_of_the_varg"]=true 
+    configuration_options={
+      hallowed_nights=true,
+      winters_feast=true,
+      year_of_the_gobbler=true,
+      year_of_the_pig=true,
+      year_of_the_varg=true 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
-  ["workshop-1580779375"]={ ["configuration_options"]={ ["Language"]="cn" }, ["enabled"]=true },
-  ["workshop-362175979"]={ ["configuration_options"]={ ["Draw over FoW"]="disabled" }, ["enabled"]=true },
-  ["workshop-374550642"]={ ["configuration_options"]={ ["MAXSTACKSIZE"]=99 }, ["enabled"]=true },
-  ["workshop-375850593"]={ ["configuration_options"]={  }, ["enabled"]=true },
+  ["workshop-1530801499"]={
+    configuration_options={ Hunger_Cost=1, Ownership=false, Sanity_Cost=1 },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
   ["workshop-378160973"]={
-    ["configuration_options"]={
-      ["ENABLEPINGS"]=true,
-      ["FIREOPTIONS"]=2,
-      ["OVERRIDEMODE"]=false,
-      ["SHAREMINIMAPPROGRESS"]=true,
-      ["SHOWFIREICONS"]=true,
-      ["SHOWPLAYERICONS"]=true,
-      ["SHOWPLAYERSOPTIONS"]=2 
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=false,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
-  ["workshop-447092740"]={ ["configuration_options"]={  }, ["enabled"]=true },
-  ["workshop-501385076"]={ ["configuration_options"]={  }, ["enabled"]=true },
-  ["workshop-514078314"]={
-    ["configuration_options"]={
-      ["EnableCosmeticMode"]=0,
-      ["EnableFlooringPlanting"]=false,
-      ["EnableGameTurfRecipes"]=1,
-      ["EnableIndividualization"]=true,
-      ["EnableNonPlayerSpeed"]=false,
-      ["EnableTurfedTurfRecipes"]=1,
-      ["InsulatingTurfs"]=1,
-      ["InsulationDuration"]=480,
-      ["RainProtectingTurfs"]=1,
-      ["SanityTurfs"]=1,
-      ["SpeedyTurfSpeeds"]=1.25,
-      ["SpeedyTurfs"]=1 
-    },
-    ["enabled"]=true 
-  },
-  ["workshop-543945797"]={
-    ["configuration_options"]={
-      ["amount_of_numbers"]="default",
-      ["display_mode"]="waving",
-      ["dmg_only"]="on",
-      ["number_size"]="default",
-      ["show_decimal_points"]="no" 
-    },
-    ["enabled"]=true 
-  },
-  ["workshop-599498678"]={ ["configuration_options"]={  }, ["enabled"]=true },
+  ["workshop-447092740"]={ configuration_options={  }, enabled=true },
+  ["workshop-501385076"]={ configuration_options={  }, enabled=true },
+  ["workshop-599498678"]={ configuration_options={  }, enabled=true },
   ["workshop-623749604"]={
-    ["configuration_options"]={
-      ["Craft"]="Normal",
-      ["Destroyable"]="DestroyByAll",
-      ["FoodSpoilage"]=1,
-      ["Language"]="En",
-      ["Position"]="Center",
-      ["Slots"]=80 
+    configuration_options={
+      Craft="Normal",
+      Destroyable="DestroyByAll",
+      FoodSpoilage=1,
+      Language="En",
+      Position="Center",
+      Slots=80 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
   ["workshop-631648169"]={
-    ["configuration_options"]={
-      ["HuiXueSuDu"]=1,
-      ["M_CeShi"]=false,
-      ["M_GongSu"]=false,
-      ["M_XueLiang"]=false,
-      ["QingChuYanShi"]=1 
+    configuration_options={
+      GlommerXue=true,
+      Klaus_sack=true,
+      Mod_Boss_1=true,
+      Mod_Boss_10=true,
+      Mod_Boss_11=true,
+      Mod_Boss_12=true,
+      Mod_Boss_13=true,
+      Mod_Boss_14=true,
+      Mod_Boss_15=true,
+      Mod_Boss_16=true,
+      Mod_Boss_17=true,
+      Mod_Boss_18=true,
+      Mod_Boss_2=true,
+      Mod_Boss_3=true,
+      Mod_Boss_4=true,
+      Mod_Boss_5=true,
+      Mod_Boss_6=true,
+      Mod_Boss_7=true,
+      Mod_Boss_8=true,
+      Mod_Boss_9=true,
+      WillowShao=true,
+      XianZuoBiao=true 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
   ["workshop-656256171"]={
-    ["configuration_options"]={
-      ["dont_give_dubloons"]=2,
-      ["give_dubloons"]=1,
-      ["position"]=1,
-      ["rec_dubloon"]=3,
-      ["rec_gold"]=3 
-    },
-    ["enabled"]=true 
+    configuration_options={ dont_give_dubloons=0, give_dubloons=0, position=3, rec_dubloon=1, rec_gold=3 },
+    enabled=true 
   },
   ["workshop-666155465"]={
-    ["configuration_options"]={
-      ["food_estimation"]=-1,
-      ["food_order"]=0,
-      ["food_style"]=0,
-      ["lang"]="auto",
-      ["show_food_units"]=-1 
+    configuration_options={
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
-  ["workshop-697356989"]={
-    ["configuration_options"]={
-      ["surf_sanity"]="n",
-      ["wcraftpot"]="y",
-      ["wilburrun"]="y",
-      ["woodlegs_luck"]=6,
-      ["woodlegs_sanity"]=-0.08 
+  ["workshop-812713308"]={ configuration_options={ fireflies_fear=222, language=0 }, enabled=true },
+  ["workshop-818038080"]={
+    configuration_options={
+      changefestivizerrecipe=0,
+      craftablespecific=true,
+      enableglobalreskins=true,
+      prototyper_module=1 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
-  ["workshop-785295023"]={
-    ["configuration_options"]={
-      ["bossres"]=true,
-      ["companion"]=true,
-      ["dist"]=2.5,
-      ["healthmul"]=-1,
-      ["language"]="AUTO",
-      ["minimapicon"]=false,
-      ["ownership"]=0,
-      ["rebounddmg"]=0,
-      ["recipe"]="normal",
-      ["recipe_door"]=true,
-      ["recipe_fence"]=true,
-      ["recipe_tool"]=true,
-      ["recipe_vanilla"]=false,
-      ["recipe_wall"]=true 
+  ["workshop-875994715"]={ configuration_options={ charcoal=true }, enabled=true },
+  ["workshop-899583698"]={
+    configuration_options={
+      Dig=false,
+      GameMode=true,
+      Hammer=false,
+      Language=false,
+      Sound=true,
+      TotooriaSpeech=1,
+      UIHeight=80,
+      UIScale=0.75 
     },
-    ["enabled"]=true 
+    enabled=true 
   },
-  ["workshop-812713308"]={
-    ["configuration_options"]={ ["fireflies_fear"]=222, ["language"]=0 },
-    ["enabled"]=true 
-  },
-  ["workshop-921583679"]={ ["configuration_options"]={  }, ["enabled"]=true } 
+  ["workshop-921583679"]={ configuration_options={  }, enabled=true } 
 }
