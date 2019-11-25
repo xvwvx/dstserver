f6c7e802647e53e4a7bee9bc9ff2af6bae84bc67
xvwvx <jangsky215@gmail.com>
2019-11-25 19:56:09 +0800

    Update modoverrides.lua

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,8 +1,18 @@
 return {
-  ["workshop-1166220990"]={ configuration_options={ attract=2, language=2 }, enabled=true },
-  ["workshop-1181077385"]={ configuration_options={  }, enabled=true },
   ["workshop-1216718131"]={ configuration_options={ clean=true, lang=true, stack=true }, enabled=true },
-  ["workshop-1217385169"]={ configuration_options={  }, enabled=true },
+  ["workshop-1275167632"]={
+    configuration_options={
+      collect_ability="on",
+      container_size="9",
+      hotkey_mypet="on",
+      level_system="on",
+      modlanguage="chinese",
+      personal_machine="on",
+      recipe_critterlab="off",
+      unique_skill="on" 
+    },
+    enabled=true 
+  },
   ["workshop-1301033176"]={ configuration_options={ LANG="simplified" }, enabled=true },
   ["workshop-1302830827"]={
     configuration_options={
@@ -18,28 +28,14 @@ return {
     configuration_options={ Hunger_Cost=1, Ownership=false, Sanity_Cost=1 },
     enabled=true 
   },
-  ["workshop-1796885039"]={
-    configuration_options={
-      Dig=false,
-      GameMode=true,
-      Hammer=false,
-      Language=true,
-      Sound=true,
-      TotooriaSpeech=1,
-      UIHeight=80,
-      UIScale=0.75 
-    },
-    enabled=true 
-  },
   ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
-  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
   ["workshop-375850593"]={ configuration_options={  }, enabled=true },
   ["workshop-378160973"]={
     configuration_options={
       ENABLEPINGS=true,
       FIREOPTIONS=2,
       OVERRIDEMODE=false,
-      SHAREMINIMAPPROGRESS=false,
+      SHAREMINIMAPPROGRESS=true,
       SHOWFIREICONS=true,
       SHOWPLAYERICONS=true,
       SHOWPLAYERSOPTIONS=2 
@@ -47,8 +43,7 @@ return {
     enabled=true 
   },
   ["workshop-447092740"]={ configuration_options={  }, enabled=true },
-  ["workshop-501385076"]={ configuration_options={  }, enabled=true },
-  ["workshop-599498678"]={ configuration_options={  }, enabled=true },
+  ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true },
   ["workshop-623749604"]={
     configuration_options={
       Craft="Normal",
@@ -62,55 +57,55 @@ return {
   },
   ["workshop-631648169"]={
     configuration_options={
-      GlommerXue=true,
-      Klaus_sack=true,
-      Mod_Boss_1=true,
-      Mod_Boss_10=true,
-      Mod_Boss_11=true,
-      Mod_Boss_12=true,
-      Mod_Boss_13=true,
-      Mod_Boss_14=true,
-      Mod_Boss_15=true,
-      Mod_Boss_16=true,
-      Mod_Boss_17=true,
-      Mod_Boss_18=true,
-      Mod_Boss_2=true,
-      Mod_Boss_3=true,
-      Mod_Boss_4=true,
-      Mod_Boss_5=true,
-      Mod_Boss_6=true,
-      Mod_Boss_7=true,
-      Mod_Boss_8=true,
-      Mod_Boss_9=true,
-      WillowShao=true,
-      XianZuoBiao=true 
+      [""]=0,
+      ["Boss公告"]=0,
+      GlommerXue=false,
+      Klaus_sack=false,
+      Mod_Boss_1=false,
+      Mod_Boss_10=false,
+      Mod_Boss_11=false,
+      Mod_Boss_12=false,
+      Mod_Boss_13=false,
+      Mod_Boss_14=false,
+      Mod_Boss_15=false,
+      Mod_Boss_16=false,
+      Mod_Boss_17=false,
+      Mod_Boss_18=false,
+      Mod_Boss_19=false,
+      Mod_Boss_2=false,
+      Mod_Boss_20=false,
+      Mod_Boss_3=false,
+      Mod_Boss_4=false,
+      Mod_Boss_5=false,
+      Mod_Boss_6=false,
+      Mod_Boss_7=false,
+      Mod_Boss_8=false,
+      Mod_Boss_9=false,
+      WillowShao=false,
+      XianZuoBiao=false,
+      ["中Boss击杀公告"]=0,
+      ["基本配置"]=0 
     },
     enabled=true 
   },
   ["workshop-656256171"]={
-    configuration_options={ dont_give_dubloons=0, give_dubloons=0, position=3, rec_dubloon=1, rec_gold=3 },
+    configuration_options={ dont_give_dubloons=2, give_dubloons=1, position=1, rec_dubloon=3, rec_gold=3 },
     enabled=true 
   },
   ["workshop-666155465"]={
     configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
       food_estimation=-1,
       food_order=0,
       food_style=0,
       lang="auto",
-      show_food_units=-1 
+      show_food_units=-1,
+      show_uses=-1 
     },
     enabled=true 
   },
   ["workshop-812713308"]={ configuration_options={ fireflies_fear=222, language=0 }, enabled=true },
-  ["workshop-818038080"]={
-    configuration_options={
-      changefestivizerrecipe=0,
-      craftablespecific=true,
-      enableglobalreskins=true,
-      prototyper_module=1 
-    },
-    enabled=true 
-  },
-  ["workshop-875994715"]={ configuration_options={ charcoal=true }, enabled=true },
-  ["workshop-921583679"]={ configuration_options={  }, enabled=true } 
+  ["workshop-921583679"]={ configuration_options={ finiteuses=3 }, enabled=true } 
 }
